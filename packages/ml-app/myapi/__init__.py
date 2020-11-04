5fc68390a62f907cec6d14ac1221577099df181e
korntewin <korntewin.b@gmail.com>
2020-11-04 14:53:58 +0700

    fix _version_ to __version__

--- packages/ml-app/myapi/__init__.py
+++ packages/ml-app/myapi/__init__.py
@@ -7,4 +7,4 @@ VERSION_FILENAME = config.PACKAGE_ROOT / 'VERSION'
 
 
 with open(VERSION_FILENAME, 'r') as version_file:
-    _version_ = version_file.read().strip() 
+    __version__ = version_file.read().strip() 
