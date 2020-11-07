b13e066317b08e683320f8ff1b473881d7daea1f
korntewin <korntewin.b@gmail.com>
2020-11-07 19:48:55 +0700

    fix diff test

--- packages/ml-app/myapi/config.py
+++ packages/ml-app/myapi/config.py
@@ -4,6 +4,7 @@ from pathlib import Path
 PACKAGE_ROOT = Path(__file__).resolve().parent.parent
 PREV_VER_PREDS_FILENAME = 'prev_version_preds.csv'
 TEST_FILENAME = 'test.csv'
+ACCETPTABLE_DIFF = 0.05
 
 
 class Config:
