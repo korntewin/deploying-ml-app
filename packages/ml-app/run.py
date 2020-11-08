e8e4fdc8b0be01740bbfd450933e8dfa80523174
korntewin <korntewin.b@gmail.com>
2020-11-08 16:10:11 +0700

    init heroku config file and fix application in Procfile

--- packages/ml-app/run.py
+++ packages/ml-app/run.py
@@ -2,7 +2,7 @@ from myapi.app import create_app
 from myapi import config
 
 
-application = create_app(config_object=config.DevelopmentConfig)
+application = create_app(config_object=config.ProductionConfig)
 
 
 if __name__ == '__main__':
